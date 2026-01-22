0b4099f462bc8936dd80524c400c1811387c7acc
philipmogul <philipp1mogul@gmail.com>
2026-01-22 16:08:00 +0300

    Laravel layouts with extend, section and include

---
+++ resources/views/about.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.index_default')
+
+@section('laraheader')
+    <h1> About Us </h1>
+
+@endsection
+
+@section('laracontent')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="container">
+                    <div class="row">About Us</div>
+                    <div class="row">
+                        <hr />
+                        <h2>
+                        This is the about page of our application. Here you can find more information about us.
+                        </h2>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('larafooter')
+    <h3> Footer Section </h3>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.index_default')
+
+@section('laraheader')
+    <h1> Welcome To The Index Page </h1>
+@endsection
+
+@section('laracontent')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="container">
+                    <div class="row">Welcome</div>
+
+                    <div class="row">
+                        <hr />
+                        <h2>
+                        Welcome to our application! Please login or register to continue.
+                        </h2>
+                        <hr />
+                        <div>
+                            <img src="{{ asset('images/ROG STRIX G16 MAIN.png') }}" alt="Products" />
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('larafooter')
+    <h3> Footer Section </h3>
+@endsection
+
+
