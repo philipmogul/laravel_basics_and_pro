b50dfb295678d0d8d4456826a51904e643c216ca
philipmogul <philipp1mogul@gmail.com>
2026-01-22 16:59:52 +0300

    Laravel migrations operations: migrate, migrate:rollback, migrate:refresh, migrate:rollback --path=

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -1,7 +1,7 @@
 <hr />
 <ul style="list-style: circle">
-    <li><a href="/home">Home</a></li>
+    <li><a href="/">Home</a></li>
     <li><a href="/about">About</a></li>
-    <li><a href="/services">Posts</a></li>
+    <li><a href="/posts">Posts</a></li>
 </ul>
 <hr /><br /><br />
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -15,7 +15,16 @@
         <p>I have created the posts using models!</p>
         <p>I have used: php artisan make:model Posts -mcr </p>
         <p>MODELS ARE USUALLY USED FOR INTERACTING WITH THE DATABASE.</p>
+        <p> In the Models/ModelName, create columns which user can interact with, 
+            using: protected $fillable = ['columnname','columnname']; </p>
         <p> THE CONTROLLER HAS ACCESS TO THE MODELS.</p>
+        <p> The next step is, GO TO MIGRATIONS, ADD COLUMNS TO THE TABLES, THEN: </p>
+        <p> php artisan migrate </p>
+        <p> If you made a mistake, you can rollback with: php artisan migrate:rollback </p>
+        <p> To roll back a specific migration from a many previous migrations, 
+            you can use: php artisan migrate:rollback --path=database/migrations/database/migrations/2026_01_22_132352_create_posts_table.php </p>
+        </p>
+        <p> To rerun all migrations, use php artisan migrate:refresh </p>
     </div>
                 </div>
             </div>
